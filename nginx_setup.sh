d0d775a5034849e97e35d450cab147541784e3c1
Decoder Praah <d3c0d3rpr44h@gmail.com>
2024-09-16 20:14:58 +0530

    Update nginx_setup.sh

--- nginx_setup.sh
+++ nginx_setup.sh
@@ -1,17 +1,15 @@
 #bin/bash
-echo "\n####### Updating package Repository ########\n"
+echo "\n######## Updating package Repository ########\n"
 sudo apt update
 
-echo "\n######## Installing Nginx #########\n"
+echo "\n######## Installing Nginx ########\n"
 sudo apt install nginx -y
 sudo apt auto-remove
 
 echo "\nTry opening Ubuntu_IP:80; You should see default Nginx Welcome Page\n"
 
-echo "\n####### Configuring your custom Nginx App -NginxApp ########\n"
-cd /var/www
-sudo mkdir NginxApp
-cd NginxApp
+echo "\n######## Configuring your custom Nginx App -NginxApp ########\n"
+sudo mkdir /var/www/NginxApp
 sudo echo "<!doctype html>
 <html>
 <head>
@@ -22,9 +20,8 @@ sudo echo "<!doctype html>
     <h1>Hello, Nginx!</h1>
     <p>Time to test</p>
 </body>
-</html>" > index.html
+</html>" > /var/www/NginxApp/index.html
 
-cd /etc/nginx/sites-enabled
 sudo echo "server {
        listen 81;
        listen [::]:81;
@@ -37,6 +34,6 @@ sudo echo "server {
        location / {
                try_files $uri $uri/ =404;
        }
-       }" >  NginxApp
+       }" >  /etc/nginx/sites-enabled/NginxApp
 sudo systemctl restart nginx
 echo "\nInitial Setup completed. \nTry accessing Ubuntu_IP:81; You should see Nginx page. Enjoy!!"
